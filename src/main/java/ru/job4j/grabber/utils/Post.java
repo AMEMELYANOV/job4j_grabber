b712da08df548ac3eadffc8df0c29387bd15b0d4
AMEMELYANOV <emelyanov.am@gmail.com>
2021-05-07 14:52:11 +0300

    2.2. Модель данных - Post. [#504024]

---
+++ src/main/java/ru/job4j/grabber/utils/Post.java
@@ -0,0 +1,42 @@
+package ru.job4j.grabber.utils;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private String name;
+    private String url;
+    private String text;
+    private LocalDateTime date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+}
